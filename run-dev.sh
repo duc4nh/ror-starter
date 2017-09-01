c93c37d57ecf2d48a5f0f3597c223ef4ffc9fc91
Duc Anh Nguyen <duc4nh@Ducs-MacBook-Pro.local>
2017-09-01 11:23:55 +0700

    @ #16 | should bundle install, create db and migration db when the container is started

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#bundle install
+bundle install
+
+bundle exec rake db:create
+bundle exec rake db:migrate
 
 bundle exec rails s -p $PORT -b '0.0.0.0'
--- run-prod.sh
+++ run-prod.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+bundle exec rake db:create
+bundle exec rake db:migrate
+
 bundle exec puma
